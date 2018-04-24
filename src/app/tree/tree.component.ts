3cb219bb70250a1a4632405bfc218e49894d7c90
mdoleh <a-mdoleh@expedia.com>
2018-04-24 16:19:18 -0700

    Track the dependency count & clear list on each new search

--- src/app/tree/tree.component.ts
+++ src/app/tree/tree.component.ts
@@ -1,22 +1,25 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { NpmService } from '../../services/npm-service/npm.service';
 import { Subject } from 'rxjs';
+import { DependencyListService } from '../../services/dependency-list/dependency-list.service';
 
 @Component({
   selector: 'app-tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.css']
 })
-export class TreeComponent implements OnInit {
+export class TreeComponent implements OnInit, OnDestroy {
   @Input() public updatePackageSubject: Subject<string>;
   private package: { name: string, version: string } = { name: "", version: "" };
   private dependencies: { name: string, version: string }[] = [];
   private error: string;
+  private dependencyCount: number;
 
-  constructor(private npmService: NpmService) {}
+  constructor(private npmService: NpmService, private dependencyListService: DependencyListService) {}
 
   ngOnInit(): void {
     this.updatePackageSubject.subscribe(packageName => {
+      this.dependencyListService.clearList();
       this.package.name = packageName;
       this.npmService.getLatestPackage(packageName)
       .then(npmPackage => {
@@ -26,6 +29,13 @@ export class TreeComponent implements OnInit {
       })
       .catch(err => this.error = "This package doesn't exist or something went wrong.");
     });
+
+    this.dependencyListService.listCount.subscribe(count => this.dependencyCount = count);
+  }
+
+  ngOnDestroy(): void {
+    this.updatePackageSubject.unsubscribe();
+    this.dependencyListService.listCount.unsubscribe();
   }
 
 }
--- src/services/dependency-list/dependency-list.service.ts
+++ src/services/dependency-list/dependency-list.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class DependencyListService {
+  public listCount: Subject<number> = new Subject();
   private dependencyList: { [key: string]: boolean } = <{[key: string]: boolean}>{};
 
   constructor() { }
 
   addToList(packageName: string, version: string): void {
     this.dependencyList[`${packageName}${version}`] = true;
+    this.listCount.next(Object.keys(this.dependencyList).length);
   }
 
   alreadyExists(packageName: string, version: string): boolean {
     return this.dependencyList[`${packageName}${version}`];
+  }
+
+  clearList(): void {
+    this.dependencyList = {};
   }
 }
