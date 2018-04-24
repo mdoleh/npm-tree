1d6a785f26e30a95247bb13ec9980c4c2a92c23c
mdoleh <a-mdoleh@expedia.com>
2018-04-24 16:43:06 -0700

    Initial color coding logic still a bit off

--- src/app/package/package.component.ts
+++ src/app/package/package.component.ts
@@ -8,8 +8,8 @@ import { DependencyListService } from '../../services/dependency-list/dependency
   styleUrls: ['./package.component.css']
 })
 export class PackageComponent implements OnInit {
-  @Input() public package: { name: string, version: string, alreadyExists: boolean };
-  private dependencies: { name: string, version: string, alreadyExists: boolean }[] = [];
+  @Input() public package: { name: string, version: string, packageExists: boolean, versionExists: boolean };
+  private dependencies: { name: string, version: string, packageExists: boolean, versionExists: boolean }[] = [];
 
   constructor(
     private npmService: NpmService,
@@ -17,14 +17,15 @@ export class PackageComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    if (this.package.alreadyExists) return;
+    if (this.package.versionExists || this.package.packageExists) return;
     this.npmService.getPackageByVersion(this.package.name, this.package.version)
       .then(npmPackage => {
         let keys = Object.keys(npmPackage && npmPackage.dependencies || {});
         this.dependencies = keys.map(key => ({ 
           name: key,
           version: npmPackage.dependencies[key],
-          alreadyExists: !!this.dependencyListService.alreadyExists(key, npmPackage.dependencies[key]) 
+          versionExists: this.dependencyListService.versionExists(key, npmPackage.dependencies[key]),
+          packageExists: this.dependencyListService.packageExists(key)
         }));
         this.dependencies.forEach(item => this.dependencyListService.addToList(item.name, item.version));
       });
--- src/services/dependency-list/dependency-list.service.ts
+++ src/services/dependency-list/dependency-list.service.ts
@@ -4,17 +4,21 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class DependencyListService {
   public listCount: Subject<number> = new Subject();
-  private dependencyList: { [key: string]: boolean } = <{[key: string]: boolean}>{};
+  private dependencyList: { [key: string]: string } = <{[key: string]: string}>{};
 
   constructor() { }
 
   addToList(packageName: string, version: string): void {
-    this.dependencyList[`${packageName}${version}`] = true;
+    this.dependencyList[packageName] = version;
     this.listCount.next(Object.keys(this.dependencyList).length);
   }
 
-  alreadyExists(packageName: string, version: string): boolean {
-    return this.dependencyList[`${packageName}${version}`];
+  versionExists(packageName: string, version: string): boolean {
+    return this.dependencyList[packageName] === version;
+  }
+
+  packageExists(packageName: string): boolean {
+    return !!this.dependencyList[packageName];
   }
 
   clearList(): void {
