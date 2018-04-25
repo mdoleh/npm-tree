b8bba0ab57ef576cc9d3e1cb0e1fddbb50301fd3
mdoleh <a-mdoleh@expedia.com>
2018-04-25 09:53:38 -0700

    Store all versions

--- src/services/dependency-list/dependency-list.service.ts
+++ src/services/dependency-list/dependency-list.service.ts
@@ -4,17 +4,21 @@ import { Subject } from 'rxjs';
 @Injectable()
 export class DependencyListService {
   public listCount: Subject<number> = new Subject();
-  private dependencyList: { [key: string]: string } = <{[key: string]: string}>{};
+  private dependencyList: { [key: string]: string[] } = <{[key: string]: string[]}>{};
 
   constructor() { }
 
   addToList(packageName: string, version: string): void {
-    this.dependencyList[packageName] = version;
+    if (this.dependencyList[packageName]) {
+      this.dependencyList[packageName].push(version);
+    } else {
+      this.dependencyList[packageName] = [version];
+    }
     this.listCount.next(Object.keys(this.dependencyList).length);
   }
 
   versionExists(packageName: string, version: string): boolean {
-    return this.dependencyList[packageName] === version;
+    return this.dependencyList[packageName] && this.dependencyList[packageName].includes(version);
   }
 
   packageExists(packageName: string): boolean {
